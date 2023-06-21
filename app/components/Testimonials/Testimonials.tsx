47626bcf531abd3f9bf474ef0a153d61295403bd
Adam <schrock-adam@hotmail.com>
2023-06-21 13:36:16 -0400

    commitX3

--- app/components/Testimonials/Testimonials.tsx
+++ app/components/Testimonials/Testimonials.tsx
@@ -3,26 +3,27 @@ import TestimonialCard from '../TestimonialCard/TestimonialCard';
 
 const Testimonials = () => {
   return (
-    <div id='Testimonials' className='testimonials-main-container'>
-      <div className='top'>
-        <h1>Reviewed by people</h1>
-        <h3>Clients Testimonials</h3>
-        <p>
-          Discover the positive impact we've made on our clients by reading
-          through their testimonials. Our clients have experienced our service
-          and results, and they're eager to share their positive experiences
-          with you.
-        </p>
-      </div>
-      <div className='bottom'>
-        <div className='left'>
-          <TestimonialCard img={'./maleavatar.jpg'} />
-        </div>
-        <div className='right'>
-          <TestimonialCard img={'./femaleavatar.jpg'} />
-        </div>
-      </div>
-    </div>
+    <div></div>
+    // <div id='Testimonials' className='testimonials-main-container'>
+    //   <div className='top'>
+    //     <h1>Reviewed by people</h1>
+    //     <h3>Clients Testimonials</h3>
+    //     <p>
+    //       Discover the positive impact we've made on our clients by reading
+    //       through their testimonials. Our clients have experienced our service
+    //       and results, and they're eager to share their positive experiences
+    //       with you.
+    //     </p>
+    //   </div>
+    //   <div className='bottom'>
+    //     <div className='left'>
+    //       <TestimonialCard img={'./maleavatar.jpg'} />
+    //     </div>
+    //     <div className='right'>
+    //       <TestimonialCard img={'./maleavatar.jpg'} />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
